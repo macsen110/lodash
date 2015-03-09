fb3589324824b68791dd95cfcfe66b0f174ba310
jdalton <john.david.dalton@gmail.com>
2015-03-08 20:19:46 -0700

    Bump to v3.2.2.

--- lodash.matchesproperty/index.js
+++ lodash.matchesproperty/index.js
@@ -1,5 +1,5 @@
 /**
- * lodash 3.2.1 (Custom Build) <https://lodash.com/>
+ * lodash 3.2.2 (Custom Build) <https://lodash.com/>
  * Build: `lodash modern modularize exports="npm" -o ./`
  * Copyright 2012-2015 The Dojo Foundation <http://dojofoundation.org/>
  * Based on Underscore.js 1.8.3 <http://underscorejs.org/LICENSE>
@@ -21,7 +21,7 @@ var baseClone = require('lodash._baseclone'),
  * @memberOf _
  * @category Utility
  * @param {Array|string} path The path of the property to get.
- * @param {*} value The value to compare.
+ * @param {*} srcValue The value to match.
  * @returns {Function} Returns the new function.
  * @example
  *
@@ -33,8 +33,8 @@ var baseClone = require('lodash._baseclone'),
  * _.find(users, _.matchesProperty('user', 'fred'));
  * // => { 'user': 'fred' }
  */
-function matchesProperty(path, value) {
-  return baseMatchesProperty(path, baseClone(value, true));
+function matchesProperty(path, srcValue) {
+  return baseMatchesProperty(path, baseClone(srcValue, true));
 }
 
 module.exports = matchesProperty;
